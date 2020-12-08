a451233c8f99aa2a0d0c1a0cc7b6091ff7fd04c5
searle927 <searle927@users.noreply.github.com>
2020-12-08 15:54:05 -0500

    fixed button

--- src/Components/WhoWeAre/HomePage.jsx
+++ src/Components/WhoWeAre/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
                         <h1>About Me</h1>
                         <img className="hpPic" src="https://i.imgur.com/zTrQZTV.jpg" alt=""></img>
                             <p className="info">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum maxime molestiae illum laudantium. Deleniti magni sequi veritatis, autem sapiente sit placeat? Provident earum excepturi ut laborum magnam ipsa soluta inventore!<br /><br /><br />
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum maxime molestiae illum laudantium. Deleniti magni sequi veritatis, autem sapiente sit placeat? Provident earum excepturi ut laborum magnam ipsa soluta inventore!<br /><br />
                             </p>
                             <a className="seeMore" href="http://www.google.com">SEE MORE</a>
                     </div><br />
@@ -23,7 +23,7 @@ function HomePage() {
                         <h1>Contact Me</h1>
                         <img className="hpPic" src="https://i.imgur.com/zTrQZTV.jpg" alt=""></img>
                             <p className="info">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga excepturi voluptatibus quae eligendi? Dolorum, repellat iusto maiores, eligendi sit libero perferendis earum quo accusantium, veritatis harum provident iure voluptatibus.<br /><br /><br />
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga excepturi voluptatibus quae eligendi? Dolorum, repellat iusto maiores, eligendi sit libero perferendis earum quo accusantium, veritatis harum provident iure voluptatibus.<br /><br />
                             </p>
                             <a className="seeMore" href="http://www.google.com">SEE MORE</a>
                     </div><br />        
@@ -31,7 +31,7 @@ function HomePage() {
                         <h1>Sessions</h1>
                         <img className="hpPic" src="https://i.imgur.com/zTrQZTV.jpg" alt=""></img>
                             <p className="info">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate tempora odit enim porro nisi ipsum provident pariatur mollitia. Commodi sapiente quibusdam aliquam quo natus perferendis accusamus, quis magnam enim non.<br /><br /><br />
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate tempora odit enim porro nisi ipsum provident pariatur mollitia. Commodi sapiente quibusdam aliquam quo natus perferendis accusamus, quis magnam enim non.<br /><br />
                             </p>
                             <a className="seeMore" href="http://www.google.com">SEE MORE</a>
                     </div>    
