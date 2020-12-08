77f03dc4afc1cf683731a4907be1905e0a61343b
searle927 <searle927@users.noreply.github.com>
2020-12-08 12:31:08 -0500

    finished the WhatWeDo shell

---
+++ src/Components/WhatWeDo/WWDBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './WWDBanner.css';
+
+function WhatWeDo() {
+    return (
+        <>
+            <div className="wwdBanner">
+                <div className="wwdBanner-info">
+                    <h1 className="wwdBanner-name">Portfolio</h1>
+                    <p className="wwdBanner-quote">My Work Gives Me Life.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WhatWeDo
--- src/Components/WhatWeDo/WhatWeDo.jsx
+++ src/Components/WhatWeDo/WhatWeDo.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './WhatWeDo.css';
+import WWDBanner from './WWDBanner'
 
 function WhatWeDo() {
     return (
-        <div>
-            <h4>What We Do!</h4>
-        </div>
+        <>
+            <WWDBanner />
+            <h2 className="whatIveDone">The work I have done this year.</h2>
+            <div className="wwdContainer">
+                <h2 className="speaker">KEYNOTE SPEAKER</h2>
+                <hr></hr>
+                <p className="speakerQuote">"You can speak well if your tongue can deliver the message of your heart"  - John Ford</p><br />
+                <img className="speakerPic" src="https://i.imgur.com/c8qta3u.jpg" alt=""></img>
+            </div>
+            {/* below i added another container. just add and get rid of whatever you need */}
+            <div className="wwdContainer2">
+                <h2 className="random">ADD INFORMATION</h2>
+                <hr></hr>
+                <p className="randomQuote">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia consequatur quaerat nobis maiores molestias perspiciatis, quo, harum provident dolorum voluptatibus laboriosam dicta, blanditiis doloribus quis eaque corrupti vel sequi commodi.</p><br />
+                <img className="randomPic" src="https://i.imgur.com/c8qta3u.jpg" alt=""></img>
+            </div>
+        </>
     )
 }
 
